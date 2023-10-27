04c188120709d37bb388864bbccee6d1730fbfee
Sulabh Prasad (Sameer) <sulabhprasad8002@gmail.com>
2023-10-27 18:19:18 +0530

    Strategy Pattern Observer Pattern

---
+++ src/main/java/ObserverPattern/CurrentConditionDisplay.java
@@ -0,0 +1,22 @@
+package ObserverPattern;
+
+public class CurrentConditionDisplay implements Observer, DisplayElement{
+    private float temperature;
+    private float humidity;
+    private  WeatherData weatherData;
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "% humidity");
+    }
+}
---
+++ src/main/java/StrategyPattern/FlyNoWay.java
@@ -0,0 +1,7 @@
+package StrategyPattern;
+
+public class FlyNoWay implements FlyBehavior{
+    public void fly() {
+        System.out.println("I cant flying!");
+    }
+}
