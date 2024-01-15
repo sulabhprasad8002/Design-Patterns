fd4ab5a9b418f787d220941d11308c03d25c9bdc
Sulabh Prasad (Sameer) <sulabhprasad8002@gmail.com>
2024-01-15 16:01:09 +0530

    Adaptor Pattern

---
+++ src/main/java/AdaptorPattern/MallardDuck.java
@@ -0,0 +1,13 @@
+package AdaptorPattern;
+
+public class MallardDuck implements Duck{
+    @Override
+    public void quack() {
+        System.out.println("quack");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm flying");
+    }
+}
---
+++ src/main/java/AdaptorPattern/README.md
@@ -0,0 +1,2 @@
+# The Adapter Pattern converts interface of a class into another interface the client expects.
+# Adapter lets classes work together that couldn't work otherwise because of incompatible interfaces
